c7fd741b262c7097dba15bbc3a3b536d0e0cc541
Fabian Groen <71462970+fabiangroen@users.noreply.github.com>
2025-11-21 21:44:09 +0100

    feat: remove unnecessary state management from cursor overlay component

--- src/components/cursorOverlay.tsx
+++ src/components/cursorOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useCursors } from "../hooks/useCursors";
 import type { Cursor } from "../types";
 
